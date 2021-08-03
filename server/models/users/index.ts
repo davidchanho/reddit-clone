004381ef547afaa2f600e78eb243432bdf9cfb5e
davidchanho <33166931+davidchanho@users.noreply.github.com>
2021-08-03 11:54:26 -0400

    remove gitignore files

--- server/models/users/index.ts
+++ server/models/users/index.ts
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   avatar: { type: String, required: true },
-  password: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   followers: [{ type: Schema.Types.ObjectId, ref: "user" }],
   subreddits: [{ type: Schema.Types.ObjectId, ref: "subreddit" }],
   bookmarks: [{ type: Schema.Types.ObjectId, ref: "post" }],
